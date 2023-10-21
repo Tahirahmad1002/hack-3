7c1d879159491e87489f59ee9e74cc441420f7f7
Tahirahmad1002 <tahirahmad1002@gmail.com>
2023-10-21 02:44:36 -0700

    1st change in dataplan.c

---
+++ dataplane.c
@@ -0,0 +1,19 @@
+// name : Tahir ahmad
+// Reg no :SP23-BSE-098
+
+#include <stdio.h>
+#include<stdlib.h>
+int main (int argv, char **argc) {
+
+
+float gb_in_plan = atof(argc[1]);
+
+float curr_day = atof(argc[2]);
+
+float gb_used = atof(argc[3]);
+
+// i convert the inputs of command line arguement into float
+
+
+
+}
