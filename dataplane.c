e5abed3b1d886d92378d418d8cc47abb771b820a
Tahirahmad1002 <tahirahmad1002@gmail.com>
2023-10-21 02:54:53 -0700

    2 in dataplan.c

--- dataplane.c
+++ dataplane.c
@@ -14,6 +14,18 @@ float gb_used = atof(argc[3]);
 
 // i convert the inputs of command line arguement into float
 
+float average = gb_in_plan/30;
+
+float rem_gb = gb_used - gb_in_plan;
+
+float user_avg = gb_used/curr_day;
+
+float rem_day = 30-curr_day;
+
+
+printf("\n%.1f days used, %.1f days remaining\n\n",curr_day,rem_day);
+
+
 
 
 }
